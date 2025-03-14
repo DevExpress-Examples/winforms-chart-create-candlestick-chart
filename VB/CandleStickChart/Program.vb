b5f830dd1ae54d9328ab42ad1e9ca1474a16e029
DevExpressExampleBot <fakeEmail>
2025-03-14 12:43:46 +0400

    Source auto update [skip ci]

--- VB/CandleStickChart/Program.vb
+++ VB/CandleStickChart/Program.vb
@@ -1,22 +1,22 @@
-﻿Imports DevExpress.XtraEditors
+Imports DevExpress.XtraEditors
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows.Forms
 
 Namespace CandleStickChart
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			WindowsFormsSettings.SetDPIAware()
-			WindowsFormsSettings.AllowDpiScale = True
-			WindowsFormsSettings.AllowAutoScale = DevExpress.Utils.DefaultBoolean.True
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
-	End Module
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call WindowsFormsSettings.SetDPIAware()
+            WindowsFormsSettings.AllowDpiScale = True
+            WindowsFormsSettings.AllowAutoScale = DevExpress.Utils.DefaultBoolean.True
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
